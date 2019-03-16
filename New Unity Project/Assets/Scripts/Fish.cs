5068f5b75bfc5c83292aa800cbe246873be2fd88
annawenger <45820274+annawenger@users.noreply.github.com>
2019-03-16 13:54:01 +0100

    fish is collectable,but canvas is still wrong

---
+++ New Unity Project/Assets/Scripts/Fish.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    GameLogic gameLogic;
+    private const string TAGPLAYER = "Player";
+    private const string TAGFLOOR = "Fish";
+
+    void Start()
+    {
+        gameLogic = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameLogic>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == TAGPLAYER && gameObject.tag == "Fish")
+        {
+            gameLogic.AddScore();
+            Debug.Log("added score");
+            Destroy(this.gameObject);
+        }
+
+        //private void OnTriggerEnter2D(Collider2D col)
+        //{
+
+        //    if (col.CompareTag("Player"))
+        //    {
+        //        if (gameLogic != null)
+        //        {
+        //            gameLogic.AddFish();
+        //            Destroy(this.gameObject);
+        //            Debug.Log("Gamelogic!=null");
+        //        }
+        //        else
+        //        {
+        //            Debug.Log("Gamelogic=null");
+        //        }
+
+        //        Debug.Log("playercollision");
+        //    }
+        //    else if (col.CompareTag("Floor"))
+        //    {
+        //        if (gameLogic == null)
+        //        {
+        //            Debug.Log("Gamelogic=null22");
+        //            return;
+        //        }
+
+        //        Destroy(this.gameObject);
+        //        Debug.Log("groundcollision");
+        //    }
+        }
+    }
---
+++ New Unity Project/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameLogic : MonoBehaviour
+{
+    private SceneLoader sceneLoader;
+    public TextMeshProUGUI Score;
+    public int scoreAmount;
+    public GameObject player;
+
+    void Start()
+    {
+        sceneLoader = GameObject.FindGameObjectWithTag("Manager").GetComponent<SceneLoader>(); //definiere Sceneloader Script den ich brauche
+    }
+
+    void Update()
+    {
+        //Score.text = scoreAmount.ToString();
+    }
+
+    //public void DecreaseTomatos()
+    //{
+    //    tomatoAmount--;
+    //    if (tomatoAmount == 0)
+    //    {
+    //        PlayerPrefs.SetInt("coinAmount", coinAmount);
+    //        sceneLoader.SwitchToScene("GameOver");
+    //    }
+    //}
+
+    public void AddScore()
+    {
+        scoreAmount++;
+    }
+}
