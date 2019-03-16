d4603557876c5e1e67cea8e0d2908fcc0c27ccf6
annawenger <45820274+annawenger@users.noreply.github.com>
2019-03-16 17:50:51 +0100

    fishes are spawning but just as long as the first one isn't collected and just in one horizontal line and way to fast

--- New Unity Project/Assets/Scripts/FishSpawning.cs
+++ New Unity Project/Assets/Scripts/FishSpawning.cs
@@ -2,13 +2,15 @@
 
 public class FishSpawning : MonoBehaviour
 {
-    public GameObject Fish;
+    public GameObject Fisch;
     public GameObject boundryR;
     public GameObject boundryL;
+    public GameObject boundryLU;
+    public GameObject boundryRU;
     float randX;
-    Vector2 whereToSpawn;
-    public float spawnRate = 2f;
-    //float nextSpawn = 0.0f;
+    float randY;
+    Vector4 whereToSpawn;
+    public float spawnRate = 3f;
     float timer;
 
     void Update()
@@ -18,46 +20,12 @@ public class FishSpawning : MonoBehaviour
 
     private void SpawnFish()
     {
-        timer = timer + Time.deltaTime;
-        Debug.Log(timer);
-        if (timer > spawnRate)
-        {
             randX = Random.Range(boundryL.transform.position.x, boundryR.transform.position.x);
-            whereToSpawn = new Vector2(randX, transform.position.y);
-            Instantiate(Fish, whereToSpawn, Quaternion.identity);
+            Debug.Log("spawn up");
+            randY = Random.Range(boundryLU.transform.position.y, boundryRU.transform.position.y);
+            whereToSpawn = new Vector4(randX, randY, transform.position.x, transform.position.y);
+            Debug.Log("fish spawn down");
+            Instantiate(Fisch, whereToSpawn, Quaternion.identity);
             timer = 0f;
-        }
     }
-
-
-
-
-    //public GameObject spawn;
-    //float nextSpawn = 1f;
-    //public float spawnRate = 2f;
-    //Vector2 whereToSpawn;
-    //float randX;
-    //float timer;
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    SpawnFish();
-    //}
-
-    //private void SpawnFish()
-    //{
-    //    timer = timer + Time.deltaTime;
-    //    // timer += Time.deltaTime
-
-    //    if (timer > spawnRate)
-    //    {
-    //        nextSpawn = Time.deltaTime + spawnRate;
-    //        randX = Random.Range(-8f, 8f);
-    //        whereToSpawn = new Vector2(randX, transform.position.y);
-    //        Instantiate(spawn, whereToSpawn, Quaternion.identity);
-    //        timer = 0f;
-    //        Debug.Log("Fishes are spawning");
-    //    }
-    //}
-}
+    }
