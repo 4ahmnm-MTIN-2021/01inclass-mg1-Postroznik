8fe1f3a9742c13002fd529651056290245599ecf
annawenger <45820274+annawenger@users.noreply.github.com>
2019-03-19 10:37:47 +0100

    created IcesheetSpawner but doesn't work yet, prefabed fish for collecting the cloned ones

---
+++ New Unity Project/Assets/Scripts/IcesheetSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcesheetSpawner : MonoBehaviour {
+
+    public GameObject Icesheet;
+    public GameObject boundryR;
+    public GameObject boundryL;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 3f;
+    float timer;
+
+    void Update()
+    {
+        SpawnIcesheet();
+    }
+
+    private void SpawnIcesheet()
+    {
+        randX = Random.Range(boundryL.transform.position.x, boundryR.transform.position.x);
+        Debug.Log("Icesheet");
+        Instantiate(Icesheet, whereToSpawn, Quaternion.identity);
+        timer = 0f;
+    }
+}
